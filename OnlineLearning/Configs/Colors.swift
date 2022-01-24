9c82c043ae844e86a0bc3e975201c2db56bc2173
Ngo Nhat <ngonhat1094@gmail.com>
2022-01-24 23:43:00 +0700

    onboarding

--- OnlineLearning/Configs/Colors.swift
+++ OnlineLearning/Configs/Colors.swift
@@ -10,4 +10,5 @@ import UIKit
 struct Colors {
     static let backgroundPrimary: UIColor = .white
     static let lbColorFirst: UIColor = UIColor(hex: 0x1F1F39)
+    static let lbColorSecond: UIColor = UIColor(hex: 0x858597)
 }
--- OnlineLearning/ViewControllers/Onboarding/OnboardingChildView.swift
+++ OnlineLearning/ViewControllers/Onboarding/OnboardingChildView.swift
@@ -12,16 +12,25 @@ class OnboardingChildView: UIViewController {
     let image: UIImageView = {
         let img = UIImageView()
         img.contentMode = .scaleAspectFit
-        img.backgroundColor = .cyan
+        
         return img
     }()
     let lbTitle: UILabel = {
         let lb = UILabel()
         lb.textColor = Colors.lbColorFirst
         lb.font = UIFont(name: Font.semibold, size: 22)
+        lb.textAlignment = .center
+        lb.numberOfLines = 0
         return lb
     }()
-    let lbDescription = UILabel()
+    let lbDescription: UILabel = {
+        let lb = UILabel(frame: CGRect(x: 0, y: 0, width: 130, height: 50))
+        lb.textColor = Colors.lbColorSecond
+        lb.font = UIFont(name: Font.regular, size: 16)
+        lb.textAlignment = .center
+        lb.numberOfLines = 0
+        return lb
+    }()
     let contentStack = UIStackView()
     
     var pageIndex: Int = 0
@@ -38,11 +47,18 @@ class OnboardingChildView: UIViewController {
         contentStack.addArrangedSubview(lbTitle)
         contentStack.addArrangedSubview(lbDescription)
         contentStack.axis = NSLayoutConstraint.Axis.vertical
+        contentStack.spacing = 20
         NSLayoutConstraint.activate([
-            contentStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            contentStack.leftAnchor.constraint(equalTo: view.leftAnchor),
-            contentStack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            contentStack.rightAnchor.constraint(equalTo: view.rightAnchor)
+//            contentStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            contentStack.leftAnchor.constraint(equalTo: view.leftAnchor),
+////            contentStack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            contentStack.rightAnchor.constraint(equalTo: view.rightAnchor),
+            contentStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            contentStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            lbDescription.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 4),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: lbDescription.trailingAnchor, multiplier:4)
         ])
+        
+//        lbTitle.labelConstraint(top: image.bottomAnchor, left: contentStack.leftAnchor, right: contentStack.rightAnchor, padding: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: -40))
     }
 }
